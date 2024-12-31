77910f64841a9a9364bcf514b6ba4dfb2e6fd056
Ivan Utsalo <uitechhub@gmail.com>
2024-12-30 21:02:30 -0500

    Optimize imports in `WeatherScreen.kt`

--- app/src/main/java/com/ivantest/nooroweather/ui/screens/weather/WeatherScreen.kt
+++ app/src/main/java/com/ivantest/nooroweather/ui/screens/weather/WeatherScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.ivantest.nooroweather.R
